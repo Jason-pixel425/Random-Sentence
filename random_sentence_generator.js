6e5acae67015fa930e1a4077751af9c7bdaa7ae7
Jason-pixel425 <jasonfitzg2798@gmail.com>
2024-06-18 17:25:44 +1000

    Added functionality to generateSentence() and added a call to it on line 9. Currently uses console.log in function instead of returning a string.

--- random_sentence_generator.js
+++ random_sentence_generator.js
@@ -3,5 +3,7 @@ function generateSentence() {
     const positive = ['courage', 'creativity', 'confidence', 'curiosity', 'honesty', 'intelligence', 'loyalty', 'respect', 'trust', 'patience', 'kindness'];
     const feeling = ['joy', 'love', 'fear', 'anger', 'sadness', 'surprise', 'excitement', 'happiness', 'peace'];
     const verb = ['act', 'run', 'scream', 'play', 'plan', 'create', 'listen', 'shop', 'leave'];
-    const negative = ['angry', 'bewildered', 'clumsy', 'defeaated', 'embarrassed', 'fierce', 'grumpy', 'jealous', 'lazy'];
+    const negative = ['angriness', 'clumsiness', 'defeat', 'embarrassment', 'grumpiness', 'jealousy', 'laziness'];
+    console.log(`To avoid the burden of ${negative[Math.floor(Math.random() * negative.length)]}, ${verb[Math.floor(Math.random() * verb.length)]} with ${feeling[Math.floor(Math.random() * feeling.length)]} to be overcome with ${positive[Math.floor(Math.random() * positive.length)]}.`);
 };
+generateSentence();
